3e8ea32536c3dc7c2ba339c2f1f77b6b96dc1bed
nounjog <pierrelt@hotmail.fr>
2013-09-25 16:31:02 +0200

    ADD-Start

---
+++ ITWEM1_Project/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ITWEM1_Project.Resources;
+
+namespace ITWEM1_Project
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
