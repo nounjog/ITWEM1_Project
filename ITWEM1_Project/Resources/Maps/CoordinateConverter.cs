3751b129cc5f3b6f5bcb582c0a59dc929fa04760
nounjog <pierrelt@hotmail.fr>
2013-10-08 01:26:01 +0200

    MOD-Maps algorythm

---
+++ ITWEM1_Project/Resources/Maps/CoordinateConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device.Location; // Provides the GeoCoordinate class.
+using Windows.Devices.Geolocation; //Provides the Geocoordinate class.
+
+
+namespace ITWEM1_Project.Resources.Maps
+{
+    class CoordinateConverter
+    {
+        public static GeoCoordinate ConvertGeocoordinate(Geocoordinate geocoordinate)
+        {
+            return new GeoCoordinate
+                (
+                geocoordinate.Latitude,
+                geocoordinate.Longitude,
+                geocoordinate.Altitude ?? Double.NaN,
+                geocoordinate.Accuracy,
+                geocoordinate.AltitudeAccuracy ?? Double.NaN,
+                geocoordinate.Speed ?? Double.NaN,
+                geocoordinate.Heading ?? Double.NaN
+                );
+        }
+    }
+}
