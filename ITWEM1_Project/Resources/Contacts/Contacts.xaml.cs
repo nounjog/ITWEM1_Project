20215e2cbf00e42a013999a135c920c521cd37ee
nounjog <pierrelt@hotmail.fr>
2013-10-15 02:38:42 +0200

    ADD-Contact web services

--- ITWEM1_Project/Resources/Contacts/Contacts.xaml.cs
+++ ITWEM1_Project/Resources/Contacts/Contacts.xaml.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using Newtonsoft.Json;
+using System.Data;
 
 
 namespace ITWEM1_Project.Resources.Contacts
@@ -16,72 +19,119 @@ namespace ITWEM1_Project.Resources.Contacts
         public Contacts()
         {
             InitializeComponent();
+
+            // Customers customers = Customers.MakeTestCustomers();
+            //StackPanel customersStackPanel = new StackPanel();
+
+
+            getContact();
         }
+        String getContact()
+        {
+            String result = "";
+
+            Uri uri = new Uri("http://pierrelt.fr/WindowsPhone/getUser.php");
+            WebClient webClient = new WebClient();
+            // Register the callback
+            webClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(webClient_DownloadStringCompleted);
+            webClient.DownloadStringAsync(uri);
+            return result;
+
+        }
+
+        void webClient_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+
+            if (e.Error == null)
+            {
+
+
+                dynamic jsonObj = JsonConvert.DeserializeObject(e.Result);
+
+foreach (var child in jsonObj.Children())
+{
+    //System.Diagnostics.Debug.WriteLine("");
+        string test = child.login.Value;
+        TextBlock customerBlock = new TextBlock();
+        customerBlock.Text = test;
+        customersStackPanel.Children.Add(customerBlock);
+
+}              
+
+                //Console.Write(test.Name);
+                /*  Customers customers = Customers.MakeTestCustomers(result);
+
+                  foreach (Customer c in customers.CustomerList)
+                  {
+                      TextBlock customerBlock = new TextBlock();
+                      customerBlock.Text = c.Name;
+                      customersStackPanel.Children.Add(customerBlock);
+                  }*/
+
+            }
+
+        }
+
 
         public class Customer
         {
             public string Name { get; set; }
-            public string Address { get; set; }
             public int ID { get; set; }
-            public Customer(string inName, string inAddress,
+            public Customer(string inName,
             int inID)
             {
                 Name = inName;
-                Address = inAddress;
                 ID = inID;
             }
+        }
+        public class Customers
+        {
+            public string Name { get; set; }
 
-            public class Customers
+
+            public Customers(string inName)
             {
-                public string Name { get; set; }
-                public Customers(string inName)
-                {
-                    Name = inName;
-                    CustomerList = new List<Customer>();
-                }
-                public List<Customer> CustomerList;
-
-
-                public static Customers MakeTestCustomers()
-                {
-
-                    string[] firstNames = new string[] { "Rob", "Jim", "Joe", "Nigel", "Sally", "Tim" };
-                    string[] lastsNames = new string[] { "Smith", "Jones", "Bloggs", "Miles", "Wilkinson", "Brown" };
-
-                    Customers result = new Customers("");
-
-                    int id = 0;
-                    foreach (string lastName in lastsNames)
-                    {
-                        foreach (string firstname in firstNames)
-                        {
-                            //Construct a customer name
-                            string name = firstname + " " + lastName;
-                            //Add the new customer to the list
-                            result.CustomerList.Add(new Customer(name,
-                            name + "'s House", id));
-                            // Increase the ID for the next customer
-                            id++;
-                        }
-                    }
-                    Customers customers = Customers.MakeTestCustomers();
-                    StackPanel customersStackPanel = new StackPanel();
-
-                    foreach (Customer c in customers.CustomerList)
-                    {
-                        TextBlock customerBlock = new TextBlock();
-                        customerBlock.Text = c.Name;
-                        customersStackPanel.Children.Add(customerBlock);
-                    }
-
-                    return result;
-                }
+                Name = inName;
+                CustomerList = new List<Customer>();
             }
+            public List<Customer> CustomerList;
+
+
+            public static Customers MakeTestCustomers(string[] firstNames)
+            {
+                Customers result = new Customers("list");
+                int id = 0;
+
+                foreach (string firstname in firstNames)
+                {
+                    //Construct a customer name
+                    string name = firstname;
+                    //Add the new customer to the list
+                    result.CustomerList.Add(new Customer(name, id));
+                    // Increase the ID for the next customer
+                    id++;
+                }
+
+                return result;
+            }
+            /*  Customers customers = Customers.MakeTestCustomers();
+              StackPanel customersStackPanel = new StackPanel();
+
+              foreach (Customer c in customers.CustomerList)
+              {
+                  TextBlock customerBlock = new TextBlock();
+                  customerBlock.Text = c.Name;
+                  customersStackPanel.Children.Add(customerBlock);
+              }
+
+              return result;
+          }*/
         }
     }
+
 }
-    
 
 
- 
+
+
 
