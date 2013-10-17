f9b2f886b30cda4a88f4604443910e672e49c7fd
Thomzoe <thms.bonte@gmail.com>
2013-10-17 02:02:27 +0200

    Modifications - Contacts

--- ITWEM1_Project/Resources/Contacts/Contacts.xaml.cs
+++ ITWEM1_Project/Resources/Contacts/Contacts.xaml.cs
@@ -11,6 +11,12 @@ using Microsoft.Phone.Shell;
 using Newtonsoft.Json;
 using System.Data;
 using System.Windows.Input;
+using Microsoft.Phone.Maps.Controls;
+using Microsoft.Phone.Maps.Services;
+using System.Device.Location; // Provides the GeoCoordinate class.
+using Windows.Devices.Geolocation; //Provides the Geocoordinate class.
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 
 
@@ -18,7 +24,7 @@ namespace ITWEM1_Project.Resources.Contacts
 {
     public partial class Contacts : PhoneApplicationPage
     {
-        bool mouseDown;
+        //bool mouseDown;
         public Contacts()
         {
             InitializeComponent();
@@ -51,45 +57,88 @@ namespace ITWEM1_Project.Resources.Contacts
 
                 dynamic jsonObj = JsonConvert.DeserializeObject(e.Result);
 
-foreach (var child in jsonObj.Children())
-{
-    //System.Diagnostics.Debug.WriteLine("");
-        string test = child.login.Value;
-        TextBlock customerBlock = new TextBlock();
-        customerBlock.Text = test;
-        customersStackPanel.Children.Add(customerBlock);
-        customerBlock.MouseLeftButtonDown += customerBlock_MouseLeftButtonDown;
-        customerBlock.MouseLeftButtonUp += customerBlock_MouseLeftButtonUp;
-        mouseDown = false;
+                foreach (var child in jsonObj.Children())
+                {
+                    //System.Diagnostics.Debug.WriteLine("");
+                    string test = child.login.Value;
+                    TextBlock customerBlock = new TextBlock();
+                    customerBlock.Text = test;
 
-}              
+                    customersStackPanel.Children.Add(customerBlock);
+                    customerBlock.Name = child.id.Value;
+                    customerBlock.MouseLeftButtonUp += customerBlock_MouseLeftButtonUp;
+                    
 
-                //Console.Write(test.Name);
-                /*  Customers customers = Customers.MakeTestCustomers(result);
-
-                  foreach (Customer c in customers.CustomerList)
-                  {
-                      TextBlock customerBlock = new TextBlock();
-                      customerBlock.Text = c.Name;
-                      customersStackPanel.Children.Add(customerBlock);
-                  }*/
-
+                }
             }
-
         }
         void customerBlock_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            if (mouseDown)
-            {
-                NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+                TextBlock customerBlock = (TextBlock)sender;
+                string id_receveur = customerBlock.Name;
+                GetCoordinates(id_receveur);
+                
             }
-            mouseDown = false;
+
+        private async void GetCoordinates(string id_receveur)
+        {
+            // Get the phone's current location.
+            Geolocator MyGeolocator = new Geolocator();
+            MyGeolocator.DesiredAccuracyInMeters = 5;
+            Geoposition MyGeoPosition = null;
+            try
+            {
+                MyGeoPosition = await MyGeolocator.GetGeopositionAsync(TimeSpan.FromMinutes(1), TimeSpan.FromSeconds(10));
+                //MyCoordinates.Add(new GeoCoordinate(MyGeoPosition.Coordinate.Latitude, MyGeoPosition.Coordinate.Longitude));
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Location is disabled in phone settings or capabilities are not checked.");
+            }
+            catch (Exception ex)
+            {
+                // Something else happened while acquiring the location.
+                MessageBox.Show(ex.Message);
+            }
+
+            String res =  webServicesConnect("http://pierrelt.fr/WindowsPhone/localisation.php?latitude="+MyGeoPosition.Coordinate.Latitude+"&longitude="+MyGeoPosition.Coordinate.Longitude+"&id="+id_receveur);
+            
+
+        }
+        String webServicesConnect(String url)
+        {
+            String result = "";
+
+            Uri uri = new Uri(url);
+            WebClient webClient = new WebClient();
+            // Register the callback
+            webClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(webClient_downloadStringCompleted);
+            webClient.DownloadStringAsync(uri);
+            return result;
+
+        }
+        void webClient_downloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+
+            if (e.Error == null)
+            {
+                String res = e.Result;
+                if ("OK".Equals(res))
+                {
+                    System.Diagnostics.Debug.WriteLine("SUCCES");
+
+                }
+            }
+
+        }
+  
+        }
         }
 
-        void customerBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            mouseDown = true;
-        }
+        //void customerBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    mouseDown = true
+        //}
 
         public class Customer
         {
@@ -145,9 +194,9 @@ foreach (var child in jsonObj.Children())
               return result;
           }*/
         }
-    }
+    
 
-}
+
 
 
 
